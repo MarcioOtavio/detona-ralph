3bf87ca979ed4825413d0ce9ce8e02692bacda91
Márcio Otávio <marcio.otaavio@gmail.com>
2023-11-14 11:53:05 -0300

    mais mudanças

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,29 @@
-alert("hello world");
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        tileLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    },
+    Values: {},
+};
+
+function randomSquare() {
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    })
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square)=>{})
+}
+
+function initialize() {
+    randomSquare();
+}
+
+initialize();
+
